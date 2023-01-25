d29c562d366d962614d4c5834752d3cdbed3b355
Muhammad Yaseen <yaseenmighty86@gmail.com>
2023-01-25 18:07:20 +0500

    JavaScript Assignment_1

---
+++ Assignment_1/assets/js/app.js
@@ -0,0 +1,29 @@
+//-------------------------- Assignment No 01 ----------------------------------//
+//-------------------------- ALERTS -------------------------------------------//
+
+// Question: 01 ->  Write a script to greet your website visitor using JS alert box.
+//alert("Hello Sir, have a good day");
+
+// Question: 02 -> Write a script to display following message on your web page:
+//alert("Error! Please enter a valid password.");
+
+// Question: 03 -> Write a script to display following message on your web page: (Hint : Use line break)
+//alert("Welcome to JS Land \n Happy Coding!");
+
+// Question: 04 -> Write a script to display following messages in sequence:
+//alert("Welcome to JS Land");
+//alert("Happy Coding!");
+
+// Question: 05 -> Generate the following message through browser’s developer console:
+//alert("Hello... I can run JS through my web browser's console");
+
+// Question: 06 -> Make use of alerts in your new/existing HTML & CSS project.
+
+// Question: 07 -> . Practice placement of <script></script> element in // following sections of your project in exercise 6:
+
+// a. Head
+// b. Body (before your page’s HTML)
+// ALERTS | JAVASCRIPT
+// Page 3 of 3
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
