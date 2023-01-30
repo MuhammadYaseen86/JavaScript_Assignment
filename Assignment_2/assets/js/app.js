ddf06b217a9ebc1bf1cd7aff51145ff0cea5674c
Muhammad Yaseen <yaseenmighty86@gmail.com>
2023-01-30 14:01:56 +0500

    JavaScript Assignment_2

---
+++ Assignment_2/assets/js/app.js
@@ -0,0 +1,106 @@
+//-------------------------- Assignment No 02 ----------------------------------//
+//-------------------------- VARIABLES FOR STRINGS -----------------------------//
+
+// Question: 01 ->  Declare a variable called username..
+//Ans: var username;
+
+// Question: 02 -> Declare a variable called myName & assign to it a string that represents your Full Name.
+//Ans: var myName = "Muhammad Yaseen";
+
+// Question: 03 -> Write script to
+//a) Declare a JS variable, titled message.
+//b) Assign “Hello World” to variable message
+//c) Display the message in alert box.
+//Ans: var title = "Hello World";
+       //alert(title);
+
+// Question: 04 ->Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+//Ans: var stdName = "Jhone Doe";
+       //var stdAge = "15 years old";
+       //var stdCourseName = "Certified Mobiel Application Development"
+	   //alert(stdName);
+	   //alert(stdAge);
+	   //alert(stdCourseName);
+
+// Question: 05 -> Write a script to display the following alert using one JS variable:
+//Ans: alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Question: 06 -> Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+//Ans: var email = "yaseenmighty86@gmail.com";
+       //alert("My email Address is" email);
+
+// Question: 07 -> Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+//Ans: var book = "A smarter way to learn JavaScript";
+       //alert(book);
+
+// Question: 08 -> Write a script to display this in browser through JS.
+//Ans: doucment.write("Yah! I can write HTML content through JavaSript");
+
+// Question: 09 -> Store following string in a variable and show in alert and browser through JS.
+//Ans: alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+
+//-------------------------- VARIABLES FOR NUMBERS -----------------------------//
+
+// Question: 01 -> Declare a variable called age & assign to it your age. Show your age in an alert box.
+//Ans: var age = 27;
+       //alert("I am " + age + " years old");
+
+// Question: 02 -> Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+//Ans: var trackUser = 14;
+	   //alert("You have visisted this site " + trackUser + " times");
+
+
+// Question: 03 -> Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+//Ans: var birthYear = 1997;
+       //document.write("My birth year is " + birthYear);
+       //document.write("Data type of my ddeclare variable is number");
+
+// Question: 04 -> A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+//Ans: 	var vistorName = "John Doe";
+		//var ProductTitle = "T-shirt(s)";
+		//var Quantity = "5";
+	    //document.write(vistorName + " ordered " + Quantity + " T-shirt(s) " + "on XYZ Clothing store");
+
+
+//-------------------------- VARIABLE NAMES: LEGAL & ILLEGAL -----------------------------//
+// Question: 01 -> Declare 3 variables in one statement.
+//Ans: var variable1, variable2, variable3;
+
+// Question: 02 -> Declare 5 legal & 5 illegal variable names.
+//Ans: var userId, fistName, lastName, age, courseName;
+       //var 123abc, person-info, #specialCharacter, $-dollar, couse-name;
+
+
+// Question: 03 -> Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+		//var number = "numbers";
+		//var dollarVar = "$";
+		//var firstVar = "$my_1stVariable";
+		//var nameDollar = "$name";
+		//var name_ = "_name";
+		//var name = "name";
+		//var caseSensitive = "name";
+		//var notBeKeyword = "Keyword";
+
+		//document.write("<h1>Rules for naming JS variables</h1>");
+		//document.write("Variable names can only contain, " + number + " , " + dollarVar +  " and _. For Example:" + firstVar + "<br>");
+		//document.write("Variables must begin with a letter, " + dollarVar + " or _. For Example: " + nameDollar + " " + " " +  name_  +" " + name + "<br>");
+		//document.write("Variable names are case " + caseSensitive + "<br>");
+		//document.write("Variable names should not be JS " + notBeKeyword);
+
+
+
+
+
+
